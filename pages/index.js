d740ce5558867eb76a923d6bf219a336b87d8f70
NAVAN <contactnavaneeth@gmail.com>
2022-07-30 14:43:32 +1000

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import fetch from 'node-fetch'
+
+export default class extends Component {	
+  render () {	  
+    return (      
+      <div>
+		  <div>{this.props.fact}</div>		  
+		  <div>{this.props.length}</div>
+	  </div>
+    )
+  }
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://catfact.ninja/fact"
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      fact: data.fact,
+      length: data.length
+    },
+  };
+}
